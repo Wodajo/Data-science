b1619da859c9c2539300cc1f8c56a7825a35bbbb
Wodajo <wodajo@pm.me>
2023-04-22 10:08:20 +0200

    pre_process done?

--- pre_process.sh
+++ pre_process.sh
@@ -1,16 +1,22 @@
 #! /usr/bin/env bash
-docker run -it --rm -v "$PWD":/media/twardovsky/sda/Mateusz_Kurzyński --log-driver none ca64a695154d bash;\
-\
-cd nanopolish;\
-\
-./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/covid1.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/covid1.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/eventalign.txt;\
-\
-./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/covid2.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/covid2.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/eventalign.txt;\
-\
-./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/patient11.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/patient11.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/eventalign.txt;\
-\
+echo "[+] starting m6anet docker container"
+docker run -it --rm -v "$PWD":/media/twardovsky/sda/Mateusz_Kurzyński --log-driver none ca64a695154d bash
+
+cd nanopolish
+
+echo "[+] nanopolish eventalign covid1"
+./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/covid1.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/covid1.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid1/eventalign.txt
+
+echo "[+] nanopolish eventalign covid2"
+./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/covid2.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/covid2.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/covid2/eventalign.txt
+
+echo "[+] nanopolish eventalign patient11"
+./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/patient11.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/patient11.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient11/eventalign.txt
+
+echo "[+] nanopolish eventalign patient14"
 ./nanopolish eventalign --reads /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient14/patient14.fq --bam /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient14/patient14.bam --genome /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/ref.fa --scale-events --signal-index --summary /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient14/final_summary.txt -t 35 > /media/twardovsky/sda/Mateusz_Kurzy\305\204ski/patient14/eventalign.txt
 
+echo "[+] pre_process.sh completed (MATEUSZ KURZYŃSKI JEST SUPER)"
 ```
 ./nanopolish index -d /media/twardovsky/sda/Mateusz_Kurzyński/covid2/fast5_pass -d /media/twardovsky/sda/Mateusz_Kurzyński/covid2/fast5_fail /media/twardovsky/sda/Mateusz_Kurzyński/covid2/covid2.fq
 ./nanopolish index -d /media/twardovsky/sda/Mateusz_Kurzyński/patient14/fast5_pass -d /media/twardovsky/sda/Mateusz_Kurzyński/patient14/fast5_fail /media/twardovsky/sda/Mateusz_Kurzyński/patient14/patient14.fq
