20a65c57ba99c1b49c19b968f8a6c6cdb7df84f2
Wodajo <wodajo@pm.me>
2023-11-15 19:23:46 +0100

    twardovsky

--- R/Rstudio.sh
+++ R/Rstudio.sh
@@ -1,26 +1,26 @@
-#! /bin/usr/env bash
-# niech argumentami beda: info dla rstudio (renv cache, project dir, password, port), dane dla tunelu ssh, 
+#! /usr/bin/env bash
 
-MAIN_LOG='Rstudio.log'
+#MAIN_LOG='Rstudio.log'
 
-if [ "$1" == "" || "$5" == ""]
+if [ "$1" == "" ] || [ "$4" == "" ]
 then
 	echo 'Syntax: Rstudio.sh /path/to/renv_cache /path/to/projects_home passwd port'
-	echo 'Logfile in Rstudio.log'
+#	echo 'Logfile in Rstudio.log'
 else
 	echo '[+] docker setup'
 	RENV_PATHS_CACHE_HOST=$1
 	RENV_PATHS_CACHE_CONTAINER=/renv/cache
 
-	docker run --rm\
-	--name rstudio_wodajo --restart=always\
-	-e USERID=$(id -u) -e GROUPID=$(id -g)\
-       	-v $1:$1 -v $2:$2\
-	-v "${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER}"\
-	-e "RENV_PATHS_CACHE"=${RENV_PATHS_CACHE_CONTAINER}\
-	-e PASSWORD=$3\
+	docker run \
+	--name rstudio_wodajo --restart=always \
+	-e USERID=$(id -u) -e GROUPID=$(id -g) \
+       	-v $1:$1 -v $2:/projects \
+	-v "${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER}" \
+	-e "RENV_PATHS_CACHE"="${RENV_PATHS_CACHE_CONTAINER}" \
+	-e PASSWORD=$3 \
 	-d \
-	-p $4\
-	R -s -e 'renv:restore()'\
-	#R -s -e 'renv:restore(); shiny::runApp(host = "0.0.0.0", port = $4)\
-	rstudio_wodajo
+	-p $4:$4 \
+	-e "R -s -e 'renv::restore()'" \
+	rstudio_wodajo_test1
+fi
+echo '[+] done'
